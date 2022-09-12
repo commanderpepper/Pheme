c733714b37ee6b26979647968b998ba0b2049b59
humza <ahmadhumza7@gmail.com>
2022-09-12 11:26:48 -0700

    Added a new UI state that will be used to display a full articles, as well as a Entity to store data.

---
+++ app/src/main/java/com/commanderpepper/pheme/room/model/ArticleEntity.kt
@@ -0,0 +1,16 @@
+package com.commanderpepper.pheme.room.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ArticleEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val publisher: String,
+    val title: String,
+    val thumbnail: String,
+    val preview: String,
+    val content: String,
+    val isFavorite: Boolean
+)
---
+++ app/src/main/java/com/commanderpepper/pheme/uistate/NewsItemUIState.kt
@@ -0,0 +1,10 @@
+package com.commanderpepper.pheme.uistate
+
+data class NewsItemUIState(
+    val publisher: String,
+    val author: String,
+    val title: String,
+    val thumbnail: String,
+    val content: String,
+    val isFavorite: Boolean
+)
