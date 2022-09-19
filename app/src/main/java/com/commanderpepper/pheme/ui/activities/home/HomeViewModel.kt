c81b2623b6621861cfdf2dd7877309ec90e9960a
humza <ahmadhumza7@gmail.com>
2022-09-19 11:33:33 -0700

    Using emit instead of value.

--- app/src/main/java/com/commanderpepper/pheme/ui/activities/home/HomeViewModel.kt
+++ app/src/main/java/com/commanderpepper/pheme/ui/activities/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.commanderpepper.pheme.ui.activities.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.commanderpepper.pheme.repository.NewsRepository
@@ -11,30 +10,49 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val newsRepository: NewsRepository,
-    private val createNewsPreviewItemUseCase: CreateNewsPreviewItemUseCase) : ViewModel() {
+    private val createNewsPreviewItemUseCase: CreateNewsPreviewItemUseCase
+) : ViewModel() {
 
     private val _homeUIStateFlow = MutableStateFlow(HomeUIState())
-    val homeUIState : StateFlow<HomeUIState> = _homeUIStateFlow
+    val homeUIState: StateFlow<HomeUIState> = _homeUIStateFlow
 
-    private var viewModelJob : Job? = null
+    private var viewModelJob: Job? = null
 
-    fun fetchNews(category: String = "America"){
+    fun fetchNews(category: String = "America") {
         // Cancel the current job if one exists
         viewModelJob?.cancel()
 
         // Create a Job and assign it to the ViewModel Job. This Job will make a call to the Repository to gather the articles.
         viewModelJob = viewModelScope.launch {
-            newsRepository.fetchNewsWithCountry(Category.NEWS).collect { status ->
-                when(status){
-                    is Status.InProgress -> _homeUIStateFlow.value = _homeUIStateFlow.value.copy(isLoading = true, isError = false)
-                    is Status.Success -> _homeUIStateFlow.value = _homeUIStateFlow.value.copy(isLoading = false, isError = false, newsPreviewList = status.data.map { createNewsPreviewItemUseCase(it) })
-                    is Status.Failure -> _homeUIStateFlow.value = _homeUIStateFlow.value.copy(isLoading = false, isError = true)
+            newsRepository.fetchNewsWithCountry(Category.NEWS).catch {
+                _homeUIStateFlow.emit(
+                    _homeUIStateFlow.value.copy(isLoading = false, isError = true)
+                )
+            }.collect { status ->
+                when (status) {
+                    is Status.InProgress ->
+                        _homeUIStateFlow.emit(
+                            _homeUIStateFlow.value.copy(
+                                isLoading = true,
+                                isError = false
+                            )
+                        )
+                    is Status.Success -> _homeUIStateFlow.emit(
+                        _homeUIStateFlow.value.copy(
+                            isLoading = false,
+                            isError = false,
+                            newsPreviewList = status.data.map { createNewsPreviewItemUseCase(it) })
+                    )
+                    is Status.Failure -> _homeUIStateFlow.emit(
+                        _homeUIStateFlow.value.copy(isLoading = false, isError = true)
+                    )
                 }
             }
         }
