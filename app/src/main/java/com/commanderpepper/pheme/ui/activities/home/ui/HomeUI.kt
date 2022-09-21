005a6224a822887216106fd020e4c3b1db535210
humza <ahmadhumza7@gmail.com>
2022-09-21 13:23:12 -0700

    Added an image to be used for the error message.

--- app/src/main/java/com/commanderpepper/pheme/ui/activities/home/ui/HomeUI.kt
+++ app/src/main/java/com/commanderpepper/pheme/ui/activities/home/ui/HomeUI.kt
@@ -1,16 +1,19 @@
 package com.commanderpepper.pheme.ui.activities.home.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -116,6 +119,8 @@ fun LoadingArticles(){
 @Composable
 fun DisplayError(){
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        val image = painterResource(id = R.drawable.pheme_portrait_)
+        Image(modifier = Modifier.clip(CircleShape), painter = image, contentDescription = "Error Image")
         Text(text = "Something went wrong")
     }
 }
