8b59bb382379d0360d20cfe1293e7fb72884b4db
humza <ahmadhumza7@gmail.com>
2022-09-21 11:50:02 -0700

    Moved Composables to its own file.

--- app/src/main/java/com/commanderpepper/pheme/ui/activities/home/HomeActivity.kt
+++ app/src/main/java/com/commanderpepper/pheme/ui/activities/home/HomeActivity.kt
@@ -5,27 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.lifecycleScope
-import com.commanderpepper.pheme.R
-import com.commanderpepper.pheme.repository.local.Category
 import com.commanderpepper.pheme.ui.activities.article.ArticleActivity
 import com.commanderpepper.pheme.ui.activities.home.theme.PhemeTheme
+import com.commanderpepper.pheme.ui.activities.home.ui.DisplayHomeActivity
 import com.commanderpepper.pheme.uistate.NewsPreviewItem
 import com.commanderpepper.pheme.uistate.NewsPreviewItemUIState
 import dagger.hilt.android.AndroidEntryPoint
@@ -63,94 +53,6 @@ class HomeActivity : ComponentActivity() {
     companion object {
         const val ARTICLE_INTENT_ID = "ArticleId"
     }
-}
-
-@Composable
-fun DisplayHomeActivity(viewModel: HomeViewModel, onArticleClicked: (Long) -> Unit, onCategoryClicked: (Category) -> Unit){
-    Scaffold(
-        bottomBar = { val color = colorResource(id = R.color.bottom_app_color)
-            HomeBottomBar(backgroundColor = color, onCategoryClicked) }
-    ) {
-        val homeUIState : HomeUIState by viewModel.homeUIState.collectAsState()
-        if(homeUIState.isError){
-            Text(text = "Something went wrong")
-        }
-        else if(homeUIState.isLoading || homeUIState.newsPreviewList.isEmpty()){
-            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                CircularProgressIndicator()
-            }
-        }
-        else if(homeUIState.newsPreviewList.isNotEmpty()){
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(homeUIState.newsPreviewList, itemContent = {
-                        item -> NewsPreviewItem(item, onClick = onArticleClicked)
-                })
-            }
-        }
-    }
-}
-
-@Composable
-fun HomeBottomBar(backgroundColor : Color, onCategoryClicked: (Category) -> Unit){
-    var category : Category by rememberSaveable { mutableStateOf(Category.NEWS) }
-    BottomAppBar(backgroundColor = backgroundColor) {
-        CategoryButton(
-            category = Category.NEWS,
-            resourceId = R.drawable.ic_news,
-            contentDescription = "News",
-            isSelected = category == Category.NEWS,
-            buttonClick = {
-                category = Category.NEWS
-                onCategoryClicked(category)
-            }
-        )
-        CategoryButton(
-            category = Category.TECH,
-            resourceId = R.drawable.ic_technology,
-            contentDescription = "Technology",
-            isSelected = category == Category.TECH,
-            buttonClick = {
-                category = Category.TECH
-                onCategoryClicked(category)
-            }
-        )
-        CategoryButton(
-            category = Category.ENTERTAINMENT,
-            resourceId = R.drawable.ic_entertainment,
-            contentDescription = "Entertainment",
-            isSelected = category == Category.ENTERTAINMENT,
-            buttonClick = {
-                category = Category.ENTERTAINMENT
-                onCategoryClicked(category)
-            }
-        )
-        CategoryButton(
-            category = Category.SPORTS,
-            resourceId = R.drawable.ic_sports,
-            contentDescription = "Sports",
-            isSelected = category == Category.SPORTS,
-            buttonClick = {
-                category = Category.SPORTS
-                onCategoryClicked(category)
-            }
-        )
-    }
-}
-
-@Composable
-private fun RowScope.CategoryButton(category: Category, resourceId: Int, contentDescription: String, isSelected: Boolean, buttonClick: (Category) -> Unit) {
-    BottomNavigationItem(
-        icon = {
-            val image = painterResource(id = resourceId)
-            Icon(image, contentDescription)
-        },
-        selectedContentColor = Color.White,
-        unselectedContentColor = Color.White.copy(alpha = 0.2f),
-        onClick = {
-            buttonClick(category)
-        },
-        selected = isSelected
-    )
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/commanderpepper/pheme/ui/activities/home/ui/HomeUI.kt
@@ -0,0 +1,109 @@
+package com.commanderpepper.pheme.ui.activities.home.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import com.commanderpepper.pheme.R
+import com.commanderpepper.pheme.repository.local.Category
+import com.commanderpepper.pheme.ui.activities.home.HomeUIState
+import com.commanderpepper.pheme.ui.activities.home.HomeViewModel
+import com.commanderpepper.pheme.uistate.NewsPreviewItem
+
+@Composable
+private fun RowScope.CategoryButton(category: Category, resourceId: Int, contentDescription: String, isSelected: Boolean, buttonClick: (Category) -> Unit) {
+    BottomNavigationItem(
+        icon = {
+            val image = painterResource(id = resourceId)
+            Icon(image, contentDescription)
+        },
+        selectedContentColor = Color.White,
+        unselectedContentColor = Color.White.copy(alpha = 0.2f),
+        onClick = {
+            buttonClick(category)
+        },
+        selected = isSelected
+    )
+}
+
+@Composable
+fun HomeBottomBar(backgroundColor : Color, onCategoryClicked: (Category) -> Unit){
+    var category : Category by rememberSaveable { mutableStateOf(Category.NEWS) }
+    BottomAppBar(backgroundColor = backgroundColor) {
+        CategoryButton(
+            category = Category.NEWS,
+            resourceId = R.drawable.ic_news,
+            contentDescription = "News",
+            isSelected = category == Category.NEWS,
+            buttonClick = {
+                category = Category.NEWS
+                onCategoryClicked(category)
+            }
+        )
+        CategoryButton(
+            category = Category.TECH,
+            resourceId = R.drawable.ic_technology,
+            contentDescription = "Technology",
+            isSelected = category == Category.TECH,
+            buttonClick = {
+                category = Category.TECH
+                onCategoryClicked(category)
+            }
+        )
+        CategoryButton(
+            category = Category.ENTERTAINMENT,
+            resourceId = R.drawable.ic_entertainment,
+            contentDescription = "Entertainment",
+            isSelected = category == Category.ENTERTAINMENT,
+            buttonClick = {
+                category = Category.ENTERTAINMENT
+                onCategoryClicked(category)
+            }
+        )
+        CategoryButton(
+            category = Category.SPORTS,
+            resourceId = R.drawable.ic_sports,
+            contentDescription = "Sports",
+            isSelected = category == Category.SPORTS,
+            buttonClick = {
+                category = Category.SPORTS
+                onCategoryClicked(category)
+            }
+        )
+    }
+}
+
+@Composable
+fun DisplayHomeActivity(viewModel: HomeViewModel, onArticleClicked: (Long) -> Unit, onCategoryClicked: (Category) -> Unit){
+    Scaffold(
+        bottomBar = { val color = colorResource(id = R.color.bottom_app_color)
+            HomeBottomBar(backgroundColor = color, onCategoryClicked) }
+    ) {
+        val homeUIState : HomeUIState by viewModel.homeUIState.collectAsState()
+        if(homeUIState.isError){
+            Text(text = "Something went wrong")
+        }
+        else if(homeUIState.isLoading || homeUIState.newsPreviewList.isEmpty()){
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                CircularProgressIndicator()
+            }
+        }
+        else if(homeUIState.newsPreviewList.isNotEmpty()){
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(homeUIState.newsPreviewList, itemContent = {
+                        item -> NewsPreviewItem(item, onClick = onArticleClicked)
+                })
+            }
+        }
+    }
+}
