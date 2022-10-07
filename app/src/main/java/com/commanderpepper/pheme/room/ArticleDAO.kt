af3fa296f87f1204af519cd8b1946efa9bc8eb12
humza <ahmadhumza7@gmail.com>
2022-10-07 09:47:32 -0700

    Added an onConflict strategy when inserting articles.

--- app/src/main/java/com/commanderpepper/pheme/room/ArticleDAO.kt
+++ app/src/main/java/com/commanderpepper/pheme/room/ArticleDAO.kt
@@ -2,6 +2,7 @@ package com.commanderpepper.pheme.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.commanderpepper.pheme.room.model.ArticleEntity
 
@@ -11,7 +12,7 @@ interface ArticleDAO {
     @Query("SELECT * FROM articleentity WHERE category = :category")
     suspend fun getArticles(category: String): List<ArticleEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertArticles(articles: List<ArticleEntity>)
 
     @Query("SELECT * FROM articleentity WHERE id = :articleId")
